030f47002fbc2f0bd90c96ba60d15b37f5d87718
Alexander Zinin <spore_09@mail.ru>
2020-02-24 20:23:58 +0900

    call all workers in index file

--- index.js
+++ index.js
@@ -1,21 +1,43 @@
 const fs = require('fs').promises;
 const { join } = require('path');
 
-const usv_com = require('./workers/usv_com');
-const greylock_com = require('./workers/greylock_com');
+const a16z = require('./workers/a16z_com');
+const accel = require('./workers/accel_com');
+const foundersfund = require('./workers/foundersfund_com');
+const greylock = require('./workers/greylock_com');
+const indiehackers = require('./workers/indiehackers_com');
+const kleinerperkins = require('./workers/kleinerperkins_com');
+const lightspeed = require('./workers/lightspeed_com');
+const nea = require('./workers/nea_com');
+const sequoiacap = require('./workers/sequoiacap_com');
+const usv = require('./workers/usv_com');
+const ycombinator = require('./workers/ycombinator_com');
+
+async function executeLoader(loader, filename) {
+  const data = await loader.loadCompaniesList();
+  await fs.writeFile(
+    join(__dirname, 'example_out', `${filename}.json`),
+    JSON.stringify(data, null, 2),
+  );
+}
 
 async function example() {
-  const usvData = await usv_com.loadCompaniesList();
-  await fs.writeFile(
-    join(__dirname, 'example_out', 'usv.json'),
-    JSON.stringify(usvData, null, 2),
-  );
+  let loaders = [
+    executeLoader(a16z, 'a16z'),
+    executeLoader(accel, 'accel'),
+    executeLoader(foundersfund, 'foundersfund'),
+    executeLoader(greylock, 'greylock'),
+    executeLoader(indiehackers, 'indiehackers'),
+    executeLoader(kleinerperkins, 'kleinerperkins'),
+    executeLoader(lightspeed, 'lightspeed'),
+    executeLoader(nea, 'nea'),
+    executeLoader(sequoiacap, 'sequoiacap'),
+    executeLoader(usv, 'usv'),
+    executeLoader(ycombinator, 'ycombinator'),
+  ];
 
-  const glData = await greylock_com.loadCompaniesList();
-  await fs.writeFile(
-    join(__dirname, 'example_out', 'greylock.json'),
-    JSON.stringify(glData, null, 2),
-  );
+  loaders = loaders.map((promise) => promise.catch(console.error));
+  await Promise.all(loaders);
 }
 
 example().catch(console.error);
